4c2dfbdabfeaa6a78382420a5fe7c71ee6a9c7d4
Alfredo Cumbe <alfredojoaocumbe@gmail.com>
2020-12-31 00:56:03 +0200

    first commit

---
+++ src/main/java/mz/learing/fidelidade/RestApplication.java
@@ -0,0 +1,9 @@
+package mz.learing.fidelidade;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("/fidelidade")
+public class RestApplication extends Application {
+  
+}
